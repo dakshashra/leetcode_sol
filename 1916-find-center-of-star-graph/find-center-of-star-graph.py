d0b1542002ad4f96b3c51bba28ce08b9790f4fbe
dakshashra <113644429+dakshashra@users.noreply.github.com>
2024-06-26 19:29:04 -0500

    Time: 588 ms (86.85%) | Memory: 50.5 MB (75.62%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -4,8 +4,7 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: int
         """
-        x=edges[0][0]
-        y=edges[0][1]
-        if x in edges[1]:
-            return x
-        return y
+        
+        if edges[0][0] in edges[1]:
+            return edges[0][0]
+        return edges[0][1]
