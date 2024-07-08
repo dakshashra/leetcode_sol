76f7edd0263219cef0231370baf4c2ea1fd1bb5c
dakshashra <113644429+dakshashra@users.noreply.github.com>
2024-07-08 10:53:23 -0500

    Time: 19 ms (44.84%) | Memory: 11.8 MB (38.89%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -7,8 +7,6 @@ class Solution(object):
         """
         l=[i for i in range(1, n+1)]
         x=0
-        ll=len(l)
-        cp=l
         for i in range(n-1):
             
             x+=(k-1)
